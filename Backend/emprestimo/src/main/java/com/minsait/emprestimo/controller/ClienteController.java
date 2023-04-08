9dbbf6eba49e03250d296eb93e6ba8f2b1ece126
Pessinaaa <pedro.pessina99@gmail.com>
2023-04-07 22:40:46 -0300

    :label: Backend :construction: Retornar clientes (GET)

--- Backend/emprestimo/src/main/java/com/minsait/emprestimo/controller/ClienteController.java
+++ Backend/emprestimo/src/main/java/com/minsait/emprestimo/controller/ClienteController.java
@@ -1,8 +1,11 @@
 package com.minsait.emprestimo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +33,10 @@ public class ClienteController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente cadastrarCliente(@Valid @RequestBody Cliente cliente) throws Exception {
 		return this.clienteService.cadastrarCliente(cliente);
+	}
+	
+	@GetMapping
+	public List<Cliente> retornarTodosClientes() {
+		return this.clienteService.retornarTodosClientes();
 	}
 }
--- Backend/emprestimo/src/main/java/com/minsait/emprestimo/service/ClienteService.java
+++ Backend/emprestimo/src/main/java/com/minsait/emprestimo/service/ClienteService.java
@@ -1,5 +1,7 @@
 package com.minsait.emprestimo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +23,9 @@ public class ClienteService {
 			return this.clienteRepository.save(cliente);
 		}
 	throw new CPFJaCadastradoException(cliente.getCPF());
+	}
+	
+	public List<Cliente> retornarTodosClientes() {
+		return this.clienteRepository.findAll();
 	}
 }
